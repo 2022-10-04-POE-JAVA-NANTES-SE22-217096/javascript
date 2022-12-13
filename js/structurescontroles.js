1df7e0134263e6a1ba99b50c83325ce1f0a3accf
omnistrain <105783100+omnistrain@users.noreply.github.com>
2022-12-13 09:47:27 +0100

    First

---
+++ js/structurescontroles.js
@@ -0,0 +1,51 @@
+//Déclare une variable
+
+var maVariable1 = 0; //attention à la porté
+let maVariable2 = 1;
+const maConstante = 2;
+
+
+
+//Structure de controle
+
+//Conditions
+
+if(condition){
+
+
+}else{
+
+
+}
+
+let resultat = condition ? vrai : false;
+
+
+//Boucles 
+
+for(let i=0; i<9; i++){
+
+}
+
+while(condition){
+
+}
+
+do{
+
+}while(condition);
+
+
+//switch
+switch(express){
+    case 'valeur':
+        //
+        break;
+    case 'valeur 2':
+        //
+        break;
+    default:
+        
+
+}
+
