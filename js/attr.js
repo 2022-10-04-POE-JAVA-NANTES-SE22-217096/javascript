8df2bf4c52f0fe45563ecbe11ae445b6a9d3bd93
omnistrain <105783100+omnistrain@users.noreply.github.com>
2022-12-14 11:38:31 +0100

    correction reception

--- js/attr.js
+++ js/attr.js
@@ -19,11 +19,33 @@ btnChangeAttr.addEventListener('click',()=>{
 let btn1        = document.querySelector('#btn1');
 let btn2        = document.querySelector('#btn2');
 
-btn1.addEventListener('click',(){
+let boite1      = document.querySelector('#boite1');
+let boite2      = document.querySelector('#boite2');
 
+let message     = "L'enveloppe est ici";
+
+
+function toggleMessage(depart, destination){
+    let idReception1 = depart.getAttribute('data-reception');
+    let idReception2 = destination.getAttribute('data-reception');
+
+    let message = document.querySelector('#'+idReception1).innerHTML;
+
+    destination.removeAttribute('disabled');
+    depart.setAttribute('disabled', 'disabled');
+
+    
+    document.querySelector('#'+idReception1).innerHTML='';
+    document.querySelector('#'+idReception2).innerHTML=message;
+
+    
+}
+
+btn1.addEventListener('click',()=>{
+    toggleMessage(boite1, boite2);
 
 });
 
 btn2.addEventListener('click',()=>{
-
-}):
+    toggleMessage(boite2, boite1);
+});
